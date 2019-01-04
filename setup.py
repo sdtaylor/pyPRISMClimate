46d25876b42b13a7811b7c3351cd3f46722428c5
Shawn Taylor <shawntaylor@weecology.org>
2019-01-04 16:44:56 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='pyPRISMClimate',
+      version='0.1.0',
+      description='An interface to the PRISM Climate data',
+      url='https://github.com/sdtaylor/pyPRISMClimate',
+      author='Shawn Taylor',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
