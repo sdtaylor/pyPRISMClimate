3f24420d537001fdba7104e72af6c3ecc0e5b467
Shawn Taylor <sdtaylor@users.noreply.github.com>
2019-09-21 18:47:57 -0400

    Improve prism_iterator() (#9)
    - improve iterator metadata parsing
    
      instead of raising an error when a weird bil file shows up,
      just log it as not parsable with a reason.
    
    - add iterator tests
    
    - make iterator recursive with glob
    
    - api and example for iterator in README

--- pyPRISMClimate/utils.py
+++ pyPRISMClimate/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+from glob import glob
 from re import match
 
 def prism_md(filename):
@@ -12,23 +13,36 @@ def prism_md(filename):
     Returns:
         dictionary of metadata
         
-        {'variable':'tmean':
+        {'variable':'tmean',
          'type':monthly,
-         'status':'stable':
+         'status':'stable',
          'date':'2017-01-01',
          'date_details':{'month':1,
                          'year':2017'}
          }
     """
+    md = {'variable':None,
+          'type':None,
+          'status':None,
+          'date':None,
+          'date_details':None,
+          'parsable':None,
+          'parse_failue':None}
+    
+    if not 'PRISM' in filename:
+        md['parsable'] = False
+        md['parse_failue'] = 'Not a PRISM bil file'
+        return md
+    
     # Cutoff any extensions and get the 6 filename parts
     #    0    1    2       3     4    5
     # PRISM_tmax_stable_4kmM2_201601_bil.zip
     filename_parts = filename.split('.')[0].split('_')
     
     if len(filename_parts) != 6:
-        raise ValueError('Unknown filename format')
-    
-    md = {}
+        md['parsable'] = False
+        md['parse_failue'] = 'Unknown filename format'
+        return md
     
     md['variable'] = filename_parts[1]
     md['status'] = filename_parts[2]
@@ -40,7 +54,9 @@ def prism_md(filename):
         d = datetime.strptime(filename_parts[4], '%Y%m')
         md['type'] = 'monthly'
     except:
-        raise ValueError('Unknown timescale')
+        md['parsable'] = False
+        md['parse_failue'] = 'Unknown PRISM file date format'
+        return md
     
     md['date'] = str(d.date())
     
@@ -52,21 +68,31 @@ def prism_md(filename):
         md['date_details'] = {'month': d.month,
                               'year' : d.year}
 
+    md['parsable'] = True
     return md
 
-def prism_iterator(path):
+def prism_iterator(path, recursive=False):
     """Returns a list of metadata for all PRISM bil files located in path
     
+    Parameters:
+        path : str
+            Path to a folder to search for PRISM files
+        
+        recursive : boolean
+            If False (default) only search in the path given, it True
+            then search the full directory tree. The metadata returned
+            will include the full path of each file regardless.
     """
-    dir_listing = os.listdir(path)
+    dir_listing = glob(path + '/**', recursive=recursive)
     
-    bil_files = [f for f in dir_listing if match('^\S*\.bil$',f)]
+    bil_file_paths = [f for f in dir_listing if match('^\S*\.bil$',f)]
     
     listing = []
-    for f in bil_files:
-        file_md = prism_md(f)
-        file_md['bil_filename'] = f
-        file_md['full_path'] = os.path.join(path, f)
+    for file_path in bil_file_paths:
+        filename = os.path.basename(file_path)
+        file_md = prism_md(filename)
+        file_md['bil_filename'] = filename
+        file_md['full_path'] = file_path
         
         listing.append(file_md)
     
---
+++ test/test_iterator.py
@@ -0,0 +1,106 @@
+import pyPRISMClimate
+from pathlib import Path
+import pytest
+
+"""
+Integration test for the iterator. When given a path this will return a list
+of dictionaries with prism metadata such as variable, month, resolution, etc.
+"""
+
+# Create some blank files to pull metadata from
+prism_filenames = ['PRISM_tmean_stable_4kmM2_200308_bil.bil',
+                   'PRISM_ppt_stable_4kmD2_20180521_bil.bil',
+                   'PRISM_tmean_stable_4kmM2_200309_bil.bil',
+                   'PRISM_tmean_stable_4kmM2_200108_bil.bil',
+                   'PRISM_tmax_stable_4kmD1_20170101_bil.bil',
+                   'PRISM_tmean_stable_4kmM2_200007_bil.bil',
+                   'PRISM_tmax_stable_4kmD1_20170104_bil.bil',
+                   'PRISM_tmean_stable_4kmM2_200307_bil.bil',
+                   'PRISM_tmax_stable_4kmD1_20170103_bil.bil',
+                   'PRISM_tmean_stable_4kmM2_200109_bil.bil',
+                   'PRISM_tmax_stable_4kmD1_20170105_bil.bil',
+                   'PRISM_tmean_stable_4kmM2_200009_bil.bil',
+                   'PRISM_ppt_stable_4kmD2_20180523_bil.bil',
+                   'PRISM_tmean_stable_4kmM2_200008_bil.bil',
+                   'PRISM_tmax_stable_4kmD1_20170102_bil.bil',
+                   'PRISM_ppt_stable_4kmD2_20180520_bil.bil',
+#                   'PRISM_tmean_30yr_normal_800mM2_annual_bil.bil',
+                   'PRISM_tmean_stable_4kmM2_200107_bil.bil',
+                   'PRISM_ppt_stable_4kmD2_20180522_bil.bil']
+
+
+def test_full_iterator(tmpdir):
+    for f in prism_filenames:
+        Path(tmpdir + f).touch()
+    
+    prism_metadata = pyPRISMClimate.utils.prism_iterator(str(tmpdir))
+    
+    # if all are parsable=True it means the metadata was fully parsed
+    assert all([f['parsable'] for f in prism_metadata])
+
+"""
+Test specific filenames for exactly matching metadata
+"""
+
+filename_test_cases = []
+
+"""
+Good filenames
+"""
+filename_test_cases.append(('PRISM_tmean_stable_4kmM2_200008_bil.bil',
+                                 {'variable':'tmean',
+                                  'type':'monthly',
+                                  'status':'stable',
+                                  'date':'2000-08-01',
+                                  'date_details':{'month': 8, 'year': 2000},
+                                  'parsable':True,
+                                  'parse_failue':None}))
+filename_test_cases.append(('PRISM_tmin_stable_4kmM2_200107_bil.bil',
+                                 {'variable':'tmin',
+                                  'type':'monthly',
+                                  'status':'stable',
+                                  'date':'2001-07-01',
+                                  'date_details':{'month': 7, 'year': 2001},
+                                  'parsable':True,
+                                  'parse_failue':None}))
+filename_test_cases.append(('PRISM_ppt_stable_4kmD2_20180520_bil.bil',
+                                 {'variable':'ppt',
+                                  'type':'daily',
+                                  'status':'stable',
+                                  'date':'2018-05-20',
+                                  'date_details':{'day' : 20, 'month': 5, 'year': 2018},
+                                  'parsable':True,
+                                  'parse_failue':None}))
+filename_test_cases.append(('PRISM_tmax_provisional_4kmD1_20170101_bil.bil',
+                                 {'variable':'tmax',
+                                  'type':'daily',
+                                  'status':'provisional',
+                                  'date':'2017-01-01',
+                                  'date_details':{'day' : 1, 'month': 1, 'year': 2017},
+                                  'parsable':True,
+                                  'parse_failue':None}))
+
+"""
+Bad filenames
+"""
+filename_test_cases.append(('some_other_bil_file.bil',
+                                 {'variable':None,
+                                  'type':None,
+                                  'status':None,
+                                  'date':None,
+                                  'date_details':None,
+                                  'parsable':False,
+                                  'parse_failue':'Not a PRISM bil file'}))
+filename_test_cases.append(('PRISM_ppt_stable_4kmD2_20180520_bil_extrainfo.bil',
+                                 {'variable':None,
+                                  'type':None,
+                                  'status':None,
+                                  'date':None,
+                                  'date_details':None,
+                                  'parsable':False,
+                                  'parse_failue':'Unknown filename format'}))    
+
+@pytest.mark.parametrize('filename, expected_metadata', filename_test_cases)
+def test_prism_filenames(filename, expected_metadata):
+    md = pyPRISMClimate.utils.prism_md(filename)
+    assert md == expected_metadata
