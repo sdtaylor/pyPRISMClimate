02dee6caf9b124614ff9eaf1d8b8fdedc98262f5
Shawn Taylor <sdtaylor@users.noreply.github.com>
2020-05-02 14:52:00 -0600

    Fix download test (#12)
    * fix download_tests
    
    - looks like the names on the ftp changed slightly so they don't
    match the names in the tests anymore.
    - this generalizes the test more, utilizing the built in
    metadata extractor
    - join the glob search path in a more formal way

--- test/test_downloads.py
+++ test/test_downloads.py
@@ -6,68 +6,126 @@ Integration tests for the 4 primary download functions.
 Will test the majory of the stuff in the package. 
 """
 
+def prism_item_in_list(qury_item, list_of_items_to_check):
+    """
+    Compare a single prism specifiation to a list of PRISM files
+    returned by pyPRISMClimate.prism_iterator
+
+    """
+    for item_to_check in list_of_items_to_check:
+        matches = [item_to_check[key] == key_contents for key, key_contents in qury_item.items()]
+        if all(matches):
+            return True 
+    return False
 
 def test_daily_single(tmpdir):
     """Download a single day"""
-    pyPRISMClimate.get_prism_daily_single(variable='tmean',
-                                          date='2017-01-01',
-                                          dest_path=tmpdir,
-                                          verbose=True)
+    to_download = {'variable':'tmean',
+                   'date'    : '2017-01-01'}
     
-    assert os.path.exists(tmpdir.join('PRISM_tmean_stable_4kmD1_20170101_bil.bil'))
+    pyPRISMClimate.get_prism_daily_single(dest_path=tmpdir,
+                                          verbose=True,
+                                          **to_download)
+    
+    folder_contents = pyPRISMClimate.prism_iterator(tmpdir)
+    
+    assert prism_item_in_list(to_download, folder_contents)
 
 def test_daily_series(tmpdir):
     """Download a series of days"""
+    
+    to_download = [{'variable':'tmax',
+                    'date'    : '2016-01-01',
+                    'date_details': {'year':2016,'month': 1, 'day' : 1},
+                    'type'    : 'daily'},
+                   {'variable':'tmax',
+                    'date'    : '2016-01-02',
+                    'date_details': {'year':2016,'month': 1, 'day' : 2},
+                    'type'    : 'daily'},
+                   {'variable':'tmax',
+                    'date'    : '2016-01-03',
+                    'date_details': {'year':2016,'month': 1, 'day' : 3},
+                    'type'    : 'daily'}]
+    
     pyPRISMClimate.get_prism_dailys(variable='tmax',
                                     min_date='2016-01-01',
                                     max_date='2016-01-03',
                                     dest_path=tmpdir,
                                     verbose=True)
     
-    to_check = ['PRISM_tmax_stable_4kmD1_20160101_bil.bil',
-                'PRISM_tmax_stable_4kmD1_20160102_bil.bil',
-                'PRISM_tmax_stable_4kmD1_20160103_bil.bil']
-    all_present = [os.path.exists(tmpdir.join(f)) for f in to_check]
+    folder_contents = pyPRISMClimate.prism_iterator(tmpdir)
     
-    assert all(all_present)
+    all_items_present = [prism_item_in_list(i, folder_contents) for i in to_download]
+    
+    assert all(all_items_present)
 
 def test_monthly_single(tmpdir):
     """Download a single month"""
+    to_download = {'variable':'tmean',
+                   'date'    : '2015-05-01',
+                   'date_details': {'year':2015,'month': 5},
+                   'type'    : 'monthly'}
+        
     pyPRISMClimate.get_prism_monthly_single(variable='tmean',
                                             year=2015,
                                             month=5,
                                             dest_path=tmpdir,
                                             verbose=True)
     
-    assert os.path.exists(tmpdir.join('PRISM_tmean_stable_4kmM2_201505_bil.bil'))
+    folder_contents = pyPRISMClimate.prism_iterator(tmpdir)
+
+    assert prism_item_in_list(to_download, folder_contents)
     
 def test_monthly_series(tmpdir):
     """Download several months"""
+    to_download = [{'variable':'tmean',
+                    'date'    : '2014-11-01',
+                    'date_details': {'year':2014,'month': 11},
+                    'type'    : 'monthly'},
+                   {'variable':'tmean',
+                    'date'    : '2014-12-01',
+                    'date_details': {'year':2014,'month': 12},
+                    'type'    : 'monthly'},
+                   {'variable':'tmean',
+                    'date'    : '2015-11-01',
+                    'date_details': {'year':2015,'month': 11},
+                    'type'    : 'monthly'},
+                   {'variable':'tmean',
+                    'date'    : '2015-12-01',
+                    'date_details': {'year':2015,'month': 12},
+                    'type'    : 'monthly'}]
+    
     pyPRISMClimate.get_prism_monthlys(variable='tmean',
                                       years=[2014,2015],
                                       months=[11,12],
                                       dest_path=tmpdir,
                                       verbose=True)
     
-    to_check = ['PRISM_tmean_stable_4kmM2_201411_bil.bil',
-                'PRISM_tmean_stable_4kmM2_201412_bil.bil',
-                'PRISM_tmean_stable_4kmM2_201511_bil.bil',
-                'PRISM_tmean_stable_4kmM2_201512_bil.bil']
+    folder_contents = pyPRISMClimate.prism_iterator(tmpdir)
     
-    all_present = [os.path.exists(tmpdir.join(f)) for f in to_check]
+    all_items_present = [prism_item_in_list(i, folder_contents) for i in to_download]
     
-    assert all(all_present)
+    assert all(all_items_present)
 
 def test_normals_1_month(tmpdir):
+    to_download = [{'variable':'ppt',
+                    'date'    : '2000-10-01',
+                    'date_details': {'month': 10},
+                    'type'    : 'monthly_normals'},
+                   {'variable':'ppt',
+                    'date'    : '2000-06-01',
+                    'date_details': {'month': 6},
+                    'type'    : 'monthly_normals'}]
+    
     pyPRISMClimate.get_prism_normals(variable='ppt',
                                      resolution='4km',
                                      months=[6,10],
+                                     annual=False,
                                      dest_path=tmpdir,
                                      verbose=True)
     
-    to_check = ['PRISM_ppt_30yr_normal_4kmM2_06_bil.bil',
-                'PRISM_ppt_30yr_normal_4kmM2_10_bil.bil']
+    folder_contents = pyPRISMClimate.prism_iterator(tmpdir)
     
-    all_present = [os.path.exists(tmpdir.join(f)) for f in to_check]
-
-    assert all(all_present)
+    all_items_present = [prism_item_in_list(i, folder_contents) for i in to_download]
+    
+    assert all(all_items_present)
