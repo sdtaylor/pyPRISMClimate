c94dd8549a716922b0cd9b8d73e76cee9c22321c
Shawn <shawntaylor@weecology.org>
2021-09-09 10:45:05 -0600

    some setup details for pypi

--- setup.py
+++ setup.py
@@ -6,11 +6,45 @@ with open('pyPRISMClimate/version.py') as fp:
     exec(fp.read(), version)
 VERSION = version['__version__']
 
+LONG_DESCRIPTION = """
+# pyPRISMClimate
+
+An interface to the PRISM Climate data with functions similar
+to the R package [prism](https://github.com/ropensci/prism)
+
+## Installation
+
+Requires python 3. No other packages needed.
+
+Install via pip
+```
+pip install pyPRISMClimate
+```
+
+Install the latest version directly from GitHub:
+```
+pip install git+git://github.com/sdtaylor/pyPRISMClimate
+```
+
+## Documentation
+
+[https://sdtaylor.github.io/pyPRISMClimate](https://sdtaylor.github.io/pyPRISMClimate)
+
+## Acknowledgments
+
+Development of this software was funded by
+[the Gordon and Betty Moore Foundation's Data-Driven Discovery Initiative](http://www.moore.org/programs/science/data-driven-discovery) through
+[Grant GBMF4563](http://www.moore.org/grants/list/GBMF4563) to Ethan P. White.
+"""
+
 setup(name='pyPRISMClimate',
       version=VERSION,
       description='An interface to the PRISM Climate data',
+      long_description = LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
       url='https://github.com/sdtaylor/pyPRISMClimate',
       author='Shawn Taylor',
       license='MIT',
+      python_requires='>=3.6.0',
       packages=find_packages(),
       zip_safe=False)
